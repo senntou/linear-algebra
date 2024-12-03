34bd2d4da42e2ab027ef484e173db5cab7813726
senntou <wataru_00117@icloud.com>
2024-12-03 19:24:53 +0900

    class2 射影勾配法の実装

---
+++ class2/main.cpp
@@ -0,0 +1,112 @@
+#include "opencv2/core/mat.hpp"
+#include "utils.h"
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cwchar>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/src/Core/Matrix.h>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <string>
+
+#define OUTPUT_DIR "output/"
+
+#define N 28 // yの次元
+#define K 5  // hの次元
+const int M = N + K - 1;
+
+Eigen::Vector<double, N * N> cv2eigen(cv::Mat &img) {
+  Eigen::Vector<double, N * N> y;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      y(i * N + j) = img.at<uchar>(i, j);
+    }
+  }
+  return y;
+}
+
+cv::Mat eigen2cv(const Eigen::Vector<double, M * M> &y) {
+  cv::Mat img = cv::Mat::zeros(M, M, CV_8UC1);
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < M; j++) {
+      img.at<uchar>(i, j) = y(i * M + j);
+    }
+  }
+  return img;
+}
+
+// f = 1/2 * ||y - Ax||^2 に対し、fの微分を計算する関数
+Eigen::Vector<double, M * M> grad_f(const Eigen::Vector<double, N * N> &y,
+                                    const Eigen::MatrixXd &A,
+                                    const Eigen::Vector<double, M * M> &x) {
+  return A.transpose() * (A * x - y);
+}
+
+// 射影勾配法
+Eigen::Vector<double, M * M>
+projection_gradient_method(const Eigen::Vector<double, N * N> &y,
+                           const Eigen::MatrixXd &A, const double mu,
+                           const int max_iter) {
+  Eigen::Vector<double, M * M> x = Eigen::Vector<double, M * M>::Zero();
+  for (int i = 0; i < M * M; i++) {
+    x(i) = 100;
+  }
+  for (int i = 0; i < max_iter; i++) {
+    x -= mu * grad_f(y, A, x);
+    for (int j = 0; j < M * M; j++) {
+      x(j) = std::max(0.0, x(j));
+    }
+  }
+  return x;
+}
+
+int main() {
+  // 画像読み込み
+  std::string fileName = "mnist_8_024.png";
+  cv::Mat img = cv::imread(fileName, cv::IMREAD_GRAYSCALE);
+  std::cout << "img size: " << img.size() << std::endl;
+
+  // Mat型からEigen::Vector型に変換 (1次元ベクトルへ)
+  Eigen::Vector<double, N * N> y = cv2eigen(img);
+
+  // フィルタ
+  Eigen::Matrix<double, K, K> h = Eigen::Matrix<double, K, K>::Ones();
+  Eigen::Vector<double, M * M> a = Eigen::Vector<double, M * M>::Zero();
+  for (int i = 0; i < K; i++) {
+    for (int j = 0; j < K; j++) {
+      a(i * M + j) = h(i, j);
+    }
+  }
+
+  // Aはhで畳み込み演算を行うための行列
+  Eigen::MatrixXd A(N * N, M * M);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      A.row(i * N + j) = a;
+      vector_right_shift(a, 1);
+      if (j == N - 1) {
+        vector_right_shift(a, K - 1);
+      }
+    }
+  }
+
+  // 初期値
+  Eigen::Vector<double, M * M> x = Eigen::Vector<double, M * M>::Zero();
+  for (int i = 0; i < M * M; i++) {
+    x(i) = 100;
+  }
+
+  // muの値を変えつつ実行
+  const int max_iter = 1000;
+  for (int i = 0; i < 10; i++) {
+    // 射影勾配法
+    double mu = 0.01 * (double)(i + 1);
+    x = projection_gradient_method(y, A, mu, max_iter);
+    // 復元画像
+    cv::Mat output = eigen2cv(x);
+    cv::imwrite("output/output_" + std::to_string(i + 1) + ".png", output);
+  }
+
+  return 0;
+}
---
+++ class2/utils.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <eigen3/Eigen/Dense>
+#include <iostream>
+
+template <typename T, int N>
+void print_vector(std::string title, const Eigen::Vector<T, N> &v) {
+  std::cout << "== Vector ==" << std::endl;
+  std::cout << title << std::endl;
+  std::cout << "size: " << v.size() << std::endl;
+  std::cout << v << std::endl;
+}
+
+template <typename T, int N, int M>
+void print_matrix(std::string title, const Eigen::Matrix<T, N, M> &m) {
+  std::cout << "== Matrix ==" << std::endl;
+  std::cout << title << std::endl;
+  std::cout << "size: " << m.rows() << "x" << m.cols() << std::endl;
+  std::cout << m << std::endl;
+}
+
+template <typename T, int N>
+void vector_right_shift(Eigen::Vector<T, N> &v, int shift) {
+  Eigen::Vector<T, N> tmp = v;
+  for (int i = 0; i < N; i++) {
+    tmp(i) = v((i - shift + N) % N);
+  }
+  v = tmp;
+}
