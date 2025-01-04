896e88322ce7eac4634cd9a52c58509f140895f3
senntou <wataru_00117@icloud.com>
2025-01-04 22:07:54 +0900

    学習できた

--- work/nn/temp.py
+++ work/nn/temp.py
@@ -14,7 +14,7 @@ def get_data():
     y_train = []
     for _ in range(NUM):
         ave = np.random.randint(0, 100)
-        var = np.random.randint(0, 100)
+        var = np.random.randint(0, 10)
         x_train.append(np.random.normal(ave, var, 16))
         y_train.append([ave, var])
 
@@ -25,12 +25,10 @@ def get_data():
 
 
 def main_printer(func):
-
     def wrapper():
         print("\n====== temp.py ======\n")
         func()
         print("\n======== end ========\n")
-
     return wrapper
 
 
@@ -38,18 +36,45 @@ def main_printer(func):
 def main():
     x_train, y_train = get_data()
 
-    # モデルの構築
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Input(shape=(16,)),
-            tf.keras.layers.Dense(16, activation="relu"),
-            tf.keras.layers.Dense(32, activation="relu"),
-            tf.keras.layers.Dense(2)
-        ]
-    )
+    model = None
+    isLoad = False
 
-    model.compile(optimizer="adam", loss="mse")
-    model.fit(x_train, y_train, epochs=10)
+    # モデルが保存されている場合
+    try:
+        model = tf.keras.models.load_model("model.keras")
+        isLoad = True
+    except:
+        pass
+
+    if not isLoad:
+        # モデルの構築
+        model = tf.keras.models.Sequential(
+            [
+                tf.keras.layers.Input(shape=(16,)),
+                tf.keras.layers.Dense(16, activation="relu"),
+                tf.keras.layers.Dense(32, activation="relu"),
+                tf.keras.layers.Dense(32, activation="relu"),
+                tf.keras.layers.Dense(16, activation="relu"),
+                tf.keras.layers.Dense(2)
+            ]
+        )
+
+        model.compile(optimizer="adam", loss="mse")
+        model.fit(x_train, y_train, epochs=100)
+
+        # modelを保存
+        model.save("model.keras")
+
+    # 学習結果
+    # model.evaluate(x_train, y_train)
+
+    # 評価
+    x_test, y_test = get_data()
+    # model.evaluate(x_test, y_test)
+
+    # x_test[0]の予測
+    print("予測値: ", model.predict(x_test[:1]))
+    print("正解値: ", y_test[:1])
 
 
 if __name__ == "__main__":
